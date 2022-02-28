aaeeea93b52fac9bd82eeb74bf35d051aec7a731
haeunko <hekoh99@naver.com>
2022-02-28 19:36:30 +0900

    sleep

--- a-dance-of-fire-and-ice/level1.py
+++ a-dance-of-fire-and-ice/level1.py
@@ -1,7 +1,16 @@
 from pynput.keyboard import Key, Controller
+import time
 
 keyboard = Controller()
 
+time.sleep(3)
+
 # press space and release
-keyboard.press(Key.space)
-keyboard.release(Key.space)
+def press_space():
+    keyboard.press(Key.space)
+    keyboard.release(Key.space)
+    print("space bar pressed")
+
+press_space()
+time.sleep(2.5)
+press_space()
