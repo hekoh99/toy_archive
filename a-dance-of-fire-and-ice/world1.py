31cfc38df6d5341314e8fe4c51369ffa693ad518
haeunko <hekoh99@naver.com>
2022-03-01 12:38:58 +0900

    anno

--- a-dance-of-fire-and-ice/world1.py
+++ a-dance-of-fire-and-ice/world1.py
@@ -22,86 +22,88 @@ def game_start():
     time.sleep(2.35) # start delay
     press_space()
 
+# up 90 degrees
 def stair_up():
-    time.sleep(degree_90) # 90 degree delay
+    time.sleep(degree_90)
     press_space()
 
-    time.sleep(degree_270) # 270 degree delay
+    time.sleep(degree_270)
     press_space()
 
+# down 90 degrees
 def stair_down():
-    time.sleep(degree_270) # 270 degree delay
+    time.sleep(degree_270)
     press_space()
 
-    time.sleep(degree_90) # 90 degree delay
+    time.sleep(degree_90)
     press_space()
 
 game_start()
 
 for i in range(30):
-    time.sleep(degree_180) # 180 degree delay
+    time.sleep(degree_180) # 180 degrees delay
     press_space()
 
 stair_down()
 
 for i in range(14):
-    time.sleep(degree_180) # 180 degree delay
+    time.sleep(degree_180)
     press_space()
 
 stair_down()
 
 for i in range(14):
-    time.sleep(degree_180) # 180 degree delay
+    time.sleep(degree_180)
     press_space()
 
 stair_down()
 
 for i in range(14):
-    time.sleep(degree_180) # 180 degree delay
+    time.sleep(degree_180)
     press_space()
 
 stair_up()
 
 for i in range(14):
-    time.sleep(degree_180) # 180 degree delay
+    time.sleep(degree_180)
     press_space()
 
 stair_up()
 
 for i in range(12):
-    time.sleep(degree_180) # 180 degree delay
+    time.sleep(degree_180)
     press_space()
 
 for i in range(2):
     stair_up()
 
 for i in range(8):
-    time.sleep(degree_180) # 180 degree delay
+    time.sleep(degree_180)
     press_space()
 
 for i in range(4):
     stair_down()
 
 for i in range(12):
-    time.sleep(degree_180) # 180 degree delay
+    time.sleep(degree_180)
     press_space()
 
 for i in range(2):
     stair_up()
 
 for i in range(8):
-    time.sleep(degree_180) # 180 degree delay
+    time.sleep(degree_180)
     press_space()
 
 for i in range(2):
     stair_down()
 
 for i in range(2):
-    time.sleep(degree_180) # 180 degree delay
+    time.sleep(degree_180)
     press_space()
 
 stair_up()
 
 for i in range(6):
-    time.sleep(1.557) # 180 degree delay
+    time.sleep(1.557) # snail
     press_space()
