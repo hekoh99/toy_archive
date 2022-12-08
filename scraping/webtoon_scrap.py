5ffc1fc4f477a6aafd99d7389ec85b3c82bf0153
haeunko <hekoh99@naver.com>
2022-12-08 13:18:29 +0900

    url

--- scraping/webtoon_scrap.py
+++ scraping/webtoon_scrap.py
@@ -2,15 +2,20 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import dload
 
-response = urlopen('https://comic.naver.com/webtoon/weekday')
+base_url = 'https://comic.naver.com'
+response = urlopen(base_url + '/webtoon/weekday')
 b_html = response.read()
 s_html = b_html.decode()
 bs = BeautifulSoup(s_html, "html.parser")
 
 webtoon_list = []
+link_dic = {}
 
 for i in bs.findAll('div', {"class":'col_inner'}):
-    webtoon_list_day = [i.find('h4').text] + [j.text for j in i.findAll('a', {"class":"title"})]
+    webtoon_list_day = [i.find('h4').text]
+    for j in i.findAll('a', {'class' : 'title'}):
+        webtoon_list_day.append(j.text)
+        link_dic[j.text] = j['href']
     webtoon_list.append(webtoon_list_day)
 
-print(webtoon_list)
+print(link_dic['윌유메리미'])
