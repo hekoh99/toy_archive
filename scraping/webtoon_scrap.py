54a4095d994aa5f95a4a7053097f7cd81fa26461
hekoh99 <hekoh99@naver.com>
2022-12-11 12:25:42 +0900

    refacto

--- scraping/webtoon_scrap.py
+++ scraping/webtoon_scrap.py
@@ -18,17 +18,26 @@ index = 1
 for i in bs.findAll('div', {"class":'col_inner'}):
     webtoon_list_day = [i.find('h4').text]
     for j in i.findAll('a', {'class' : 'title'}):
-        info = [j.text]
+        # info = [j.text]
+        webtoon_list.append(j.text)
         link_dic[j.text] = j['href']
-        webtoon_res = urlopen(base_url + j['href'])
-        webtoon_page = BeautifulSoup(webtoon_res.read().decode(), "html.parser")
-        for k in webtoon_page.findAll('div', {"class" : "rating_type"}) :
-            result.append(info + [k.strong.text])
-        for k in webtoon_page.findAll('td', {"class" : "num"}):
-            result[index].append(k.text)
-            index += 1
+        # webtoon_res = urlopen(base_url + j['href'])
+        # webtoon_page = BeautifulSoup(webtoon_res.read().decode(), "html.parser")
+        # for k in webtoon_page.findAll('div', {"class" : "rating_type"}) :
+        #     result.append(info + [k.strong.text])
+        # for k in webtoon_page.findAll('td', {"class" : "num"}):
+        #     result[index].append(k.text)
+        #     index += 1
 
-with open("webtoon.csv", "w", newline="") as f:
-    wr = csv.writer(f)
-    for row in result:
-        wr.writerow(row)
+for toon in webtoon_list:
+    if toon not in link_dic:
+        continue
+    else :
+        toon_link = link_dic.pop(toon)
+        toon_res = urlopen(base_url + toon_link)
+        toon_page = BeautifulSoup(toon_res.read().decode(), "html.parser")
+
+# with open("webtoon.csv", "w", newline="") as f:
+#     wr = csv.writer(f)
+#     for row in result:
+#         wr.writerow(row)
