284d9d18ca8e9cfc27bc66427a71164b6cc2296e
hekoh99 <hekoh99@naver.com>
2022-12-11 16:40:20 +0900

    check duplicate

--- scraping/webtoon_scrap.py
+++ scraping/webtoon_scrap.py
@@ -11,33 +11,27 @@ bs = BeautifulSoup(s_html, "html.parser")
 webtoon_list = []
 link_dic = {}
 result = [["웹툰명", "평점", "날짜"]]
-index = 1
-
-# 3중 for문 어떻게 좀 해보기 (o)
 
 for i in bs.findAll('div', {"class":'col_inner'}):
     webtoon_list_day = [i.find('h4').text]
     for j in i.findAll('a', {'class' : 'title'}):
-        webtoon_list.append(j.text)
-        link_dic[j.text] = j['href']
+        try :
+            check_duplicate = link_dic[j.text] # 중복 체크
+        except KeyError:
+            link_dic[j.text] = j['href']
+            toon_res = urlopen(base_url + j['href'])
+            toon_page = BeautifulSoup(toon_res.read().decode(), "html.parser")
+            for cell in toon_page.findAll('tr'):
+                rate = cell.find('div', {"class" : "rating_type"})
+                date = cell.find('td', {"class" : "num"})
+                if rate is not None and date is not None :
+                    info = [j.text]
+                    info.append(rate.strong.text)
+                    info.append(date.text)
+                    result.append(info)
 
-for toon in webtoon_list:
-    if toon not in link_dic:
-        continue
-    else :
-        toon_link = link_dic.pop(toon)
-        toon_res = urlopen(base_url + toon_link)
-        toon_page = BeautifulSoup(toon_res.read().decode(), "html.parser")
-        for cell in toon_page.findAll('tr'):
-            rate = cell.find('div', {"class" : "rating_type"})
-            date = cell.find('td', {"class" : "num"})
-            if rate is not None and date is not None:
-                info = [toon]
-                info.append(rate.strong.text)
-                info.append(date.text)
-                result.append(info)
 
-with open("webtoon.csv", "w", newline="") as f:
+with open("result.csv", "w", newline="") as f:
     wr = csv.writer(f)
     for row in result:
         wr.writerow(row)
