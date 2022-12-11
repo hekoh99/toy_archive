97d023108022da30c6801543640b4a6a59122ba9
hekoh99 <hekoh99@naver.com>
2022-12-11 13:28:34 +0900

    refacto

--- scraping/webtoon_scrap.py
+++ scraping/webtoon_scrap.py
@@ -13,21 +13,13 @@ link_dic = {}
 result = [["웹툰명", "평점", "날짜"]]
 index = 1
 
-# 3중 for문 어떻게 좀 해보기
+# 3중 for문 어떻게 좀 해보기 (o)
 
 for i in bs.findAll('div', {"class":'col_inner'}):
     webtoon_list_day = [i.find('h4').text]
     for j in i.findAll('a', {'class' : 'title'}):
-        # info = [j.text]
         webtoon_list.append(j.text)
         link_dic[j.text] = j['href']
-        # webtoon_res = urlopen(base_url + j['href'])
-        # webtoon_page = BeautifulSoup(webtoon_res.read().decode(), "html.parser")
-        # for k in webtoon_page.findAll('div', {"class" : "rating_type"}) :
-        #     result.append(info + [k.strong.text])
-        # for k in webtoon_page.findAll('td', {"class" : "num"}):
-        #     result[index].append(k.text)
-        #     index += 1
 
 for toon in webtoon_list:
     if toon not in link_dic:
@@ -36,8 +28,16 @@ for toon in webtoon_list:
         toon_link = link_dic.pop(toon)
         toon_res = urlopen(base_url + toon_link)
         toon_page = BeautifulSoup(toon_res.read().decode(), "html.parser")
+        for cell in toon_page.findAll('tr'):
+            rate = cell.find('div', {"class" : "rating_type"})
+            date = cell.find('td', {"class" : "num"})
+            if rate is not None and date is not None:
+                info = [toon]
+                info.append(rate.strong.text)
+                info.append(date.text)
+                result.append(info)
 
-# with open("webtoon.csv", "w", newline="") as f:
-#     wr = csv.writer(f)
-#     for row in result:
-#         wr.writerow(row)
+with open("webtoon.csv", "w", newline="") as f:
+    wr = csv.writer(f)
+    for row in result:
+        wr.writerow(row)
